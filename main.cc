b6806d5780e925e72305156cdc65ea4b7cbe7f85
Sunil <sunhick@gmail.com>
2017-11-20 03:10:33 -0800

    Added all files

--- main.cc
+++ main.cc
@@ -1,9 +1,19 @@
+#include "util.h"
+#include "math/math.h"
+#include "graphics/graphics.h"
+
 #include <iostream>
 
 using namespace std;
 
-int main(int argc, char *argv[argc])
-{
+int main(int argc, char *argv[argc]) {
   cout << "Hello world from main" << endl;
+  util::console c("runner");
+  c.print("writing using console");
+
+  graphics_init();
+  
+  math::adder a;
+  cout << "10 + 90 = " << a.add(10, 90) << endl;
   return 0;
 }
